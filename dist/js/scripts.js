23ef63cc503a3d69e4569c45329576d7f11197c8
Wedhimd <wedhimd@gmail.com>
2021-07-10 17:22:05 +0200

    testing screen size ditection script

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,7 +3,22 @@ const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElements = document.querySelectorAll(".has-fade");
+const carouselHero = document.getElementById("#carouselHero");
 
+// carousel
+
+const screenSize = matchMedia("(max-width: 1023px)") 
+
+function myFunction(screenSize) {
+    if (screenSize.matches) { // If media query matches
+      document.body.style.backgroundColor = "yellow";
+    } else {
+      document.body.style.backgroundColor = "pink";
+    }
+  }
+  myFunction(screenSize)
+//   screenSize.addEventListener(myFunction)
+//carousel
 btnHamburger.addEventListener("click", function(event){
     event.stopPropagation();
     if(header.classList.contains("open")){ // close hamburger menu
